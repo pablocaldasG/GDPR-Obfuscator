936601669d459e8c9ce2ac97438f589cbe781379
pablocaldasG <90caldas@gmail.com>
2025-10-18 00:50:02 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 #import boto3
 #import pandas as pd
 
-def main():
-    print("Environment OK, boto3 and pandas loaded successfully.")
+#def main():
+    #print("Environment OK, boto3 and pandas loaded successfully.")
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+    #main()
